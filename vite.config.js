2f84f72c3f073fbbf3d0fe3313feac8d3800385f
vikash_shandilya <52299761+Vikash-shandilya@users.noreply.github.com>
2025-12-02 08:22:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-```
-
-### **6. .gitignore**
-```
-# Logs
-logs
-*.log
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-pnpm-debug.log*
-lerna-debug.log*
-
-node_modules
-dist
-dist-ssr
-*.local
-
-# Editor directories and files
-.vscode/*
-!.vscode/extensions.json
-.idea
-.DS_Store
-*.suo
-*.ntvs*
-*.njsproj
-*.sln
-*.sw?
